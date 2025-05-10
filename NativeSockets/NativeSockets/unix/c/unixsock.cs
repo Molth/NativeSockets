ce73c95b43de045fd5630308307322d19f942352
Molth Nevin <qq2201591870@163.com>
2025-05-10 14:16:24 +0800

    sin6_scope_id

--- NativeSockets/NativeSockets/unix/c/unixsock.cs
+++ NativeSockets/NativeSockets/unix/c/unixsock.cs
@@ -396,7 +396,7 @@ namespace unixsock
                 else if (addressStorage.ss_family == (int)ADDRESS_FAMILY_INTER_NETWORK_V6)
                 {
                     sockaddr_in6* __socketAddress_native = (sockaddr_in6*)&addressStorage;
-                    Unsafe.CopyBlockUnaligned(socketAddress->sin6_addr, __socketAddress_native->sin6_addr, 16);
+                    Unsafe.CopyBlockUnaligned(socketAddress->sin6_addr, __socketAddress_native->sin6_addr, 20);
                     socketAddress->sin6_port = WinSock2.NET_TO_HOST_16(__socketAddress_native->sin6_port);
                 }
             }
@@ -439,7 +439,7 @@ namespace unixsock
                 else if (addressStorage.ss_family == (int)ADDRESS_FAMILY_INTER_NETWORK_V6)
                 {
                     sockaddr_in6* __socketAddress_native = (sockaddr_in6*)&addressStorage;
-                    Unsafe.CopyBlockUnaligned(socketAddress->sin6_addr, __socketAddress_native->sin6_addr, 16);
+                    Unsafe.CopyBlockUnaligned(socketAddress->sin6_addr, __socketAddress_native->sin6_addr, 20);
                     socketAddress->sin6_port = WinSock2.NET_TO_HOST_16(__socketAddress_native->sin6_port);
                 }
             }
@@ -619,7 +619,7 @@ namespace unixsock
                     {
                         sockaddr_in6* __socketAddress_native = (sockaddr_in6*)hint->ai_addr;
 
-                        Unsafe.CopyBlockUnaligned(pAddrBuf, __socketAddress_native->sin6_addr, 16);
+                        Unsafe.CopyBlockUnaligned(pAddrBuf, __socketAddress_native->sin6_addr, 20);
 
                         freeaddrinfo(results);
 
